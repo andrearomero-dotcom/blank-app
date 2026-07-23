a1d0d9f446b5a007bf1ef0b393716a1af3cfe9e5
andrearomero-dotcom <andrea.romero@indegene.com>
2026-07-23 11:40:15 -0500

    Fix variable name for address line in context

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ email_line = f'var emailAddress = ctx.{ctx_emailAddress};' if ctx_emailAddress e
 phonenumber_line = f'var phonenumber = ctx.{ctx_phonenumber};' if ctx_phonenumber else 'var phonenumber = "";'
 stateCode_line = f'var stateCode = ctx.{ctx_stateCode};' if ctx_stateCode else 'var stateCode = "";'
 zipCode_line = f'var zip = ctx.{ctx_zip};' if ctx_zip else 'var zip = "";'
-address_line = f'var AddressLine = ctx.{ctx_AddressLine1};' if ctx_AddressLine1 else 'var AddressLine = "";'
+address_line = f'var AddressLine1 = ctx.{ctx_AddressLine1};' if ctx_AddressLine1 else 'var AddressLine1 = "";'
 
 # zipCode array
 zipCode_array = ""
@@ -88,7 +88,7 @@ addresses_array = ""
 if ctx_AddressLine1:
     addresses_array = f"""var addresses = new ArrayList();  
                 addresses.add({{
-                    "AddressLine1": AddressLine,
+                    "AddressLine1": AddressLine1,
                     "AddressType": "",
 					"StateProvince": stateCode,
 					"Zip": zipCode,
